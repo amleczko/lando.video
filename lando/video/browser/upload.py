d559144eadfe754770fc509b69984efc5a417938
Andrew Mleczko <andrew@mleczko.net>
2012-05-10 12:21:54 +0200

    fix session handling

--- lando/video/browser/upload.py
+++ lando/video/browser/upload.py
@@ -2,17 +2,14 @@
 
 import urllib2
 import socket
-import Cookie
-
-from zope.component import queryUtility
-from plone.registry.interfaces import IRegistry
 
 from Products.Five.browser import BrowserView
+from zope.component import queryUtility
+from plone.registry.interfaces import IRegistry
 
 from lando.video.interfaces import IMultiplerSettings
 
 
-LANDO_PROXY_SESSION = 'session'
 PLONE_PROXY_SESSION = 'lando.proxy.session'
 
 
@@ -25,39 +22,26 @@ class VideoUpload(BrowserView):
         return registry.forInterface(IMultiplerSettings, check=False)
 
     def get_form_url(self):
-        session, token = self.get_token()
+        token = self.get_token()
         settings = self.multipler_settings
-
-        if session:
-            print 'settings session %s' % session
-            return "%s/%s/upload?set_session=%s" % (settings.lando_url, token, session)
-        else:
-            return "%s/%s/upload" % (settings.lando_url, token)
+        return "%s/%s/upload" % (settings.lando_url, token)
 
     def get_token(self):
-        settings = self.multipler_settings
         sdm = self.context.session_data_manager
+        session = sdm.getSessionData(create=True)
+        token = session.get(PLONE_PROXY_SESSION, None)
+        if token:
+            return token
+
+        settings = self.multipler_settings
 
         dt = socket.getdefaulttimeout()
         socket.setdefaulttimeout(2)
         opener = urllib2.build_opener()
 
-        session = sdm.getSessionData(create=True)
-        plone_session = session.get(PLONE_PROXY_SESSION)
-        if plone_session:
-            opener.addheaders.append(('Cookie', '%s=%s' % (LANDO_PROXY_SESSION, plone_session)))
-
         req = opener.open('%s/get_token?token_pass=%s' % (settings.lando_url, settings.lando_pass))
         socket.setdefaulttimeout(dt)
         token = req.read()
+        session.set(PLONE_PROXY_SESSION, token)
 
-        #trying to update plone session with pyramid session id
-        cookie = Cookie.SimpleCookie()
-        cookie.load(req.headers.get('Set-Cookie',''))
-        try:
-            proxy_session = cookie[LANDO_PROXY_SESSION].value
-            session.set(PLONE_PROXY_SESSION, proxy_session)
-        except KeyError:
-            proxy_session = None
-
-        return proxy_session, token
+        return token
